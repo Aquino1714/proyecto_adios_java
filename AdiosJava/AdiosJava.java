f6269f1924ba8791d1d24e801e3ece57597895ea
Aquino1714 <asaoscarblan01@gmail.com>
2026-05-13 14:06:02 -0600

    Agregar metodo de resta y llamado al main

--- AdiosJava/AdiosJava.java
+++ AdiosJava/AdiosJava.java
@@ -9,5 +9,20 @@ public class AdiosJava {
         System.out.println("Ingrese el segundo numero2: ");
         numero2 = scanner.nextInt();
         System.out.println("La suma de los dos numeros es: " + (numero1 + numero2));
+
+        // Llamar al metodo restar
+        System.out.println("La resta de los dos numeros es: " + restar(numero1, numero2));
+    }
+
+
+    // Metodo para restar dos numeros
+    public static int restar(int numero1, int numero2) {
+        return numero1 - numero2;
     }
 }
+
+//1.- agregar un metodo que reste dos numeros (llamarlo en main)
+//2.- commit (git add, git commit, git push)
+//3.- Agregar la clase persona del proyecto
+//4.- commit (git add, git commit, git push)
+ 
