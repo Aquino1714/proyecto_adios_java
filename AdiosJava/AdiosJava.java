276793aed237ca05ffc7c4b4677b9d420ca33711
Aquino1714 <asaoscarblan01@gmail.com>
2026-05-13 23:54:27 -0600

    Se agrega clase Persona

--- AdiosJava/AdiosJava.java
+++ AdiosJava/AdiosJava.java
@@ -1,6 +1,35 @@
 package Java.AdiosJava;
 import java.util.Scanner;
 public class AdiosJava {
+
+    // Clase Persona
+    public class Persona {
+        String nombre;
+        int edad;
+
+        public Persona(String nombre, int edad) {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        // Getters y Setters
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+    }
+
+
     public static void main(String[] args) {
         int numero1, numero2;
         Scanner scanner = new Scanner(System.in);
@@ -12,6 +41,17 @@ public class AdiosJava {
 
         // Llamar al metodo restar
         System.out.println("La resta de los dos numeros es: " + restar(numero1, numero2));
+
+        //Utilizar la clase Persona 
+        Persona persona1 = new Persona();
+        
+        // Usando SETTERS para asignar valores
+        persona1.setNombre("Carlos");
+        persona1.setEdad(25);
+        
+        // Obteniendo y mostrando los valores
+        System.out.println("Nombre de la persona: " + persona1.getNombre());
+        System.out.println("Edad de la persona: " + persona1.getEdad());
     }
 
 
